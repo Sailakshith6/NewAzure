2b2de992eb8228d9543386b88edd8faa1ed57177
Sailakshith6 <117815467+Sailakshith6@users.noreply.github.com>
2024-10-29 16:35:32 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,36 +1,66 @@
 variable "subscription_id" {
-  description = "The subscription ID for Azure."
+  description = "The subscription ID for the Azure account."
   type        = string
 }
 
 variable "client_id" {
-  description = "The client ID for Azure service principal."
+  description = "The client ID for Azure authentication."
   type        = string
 }
 
 variable "client_secret" {
-  description = "The client secret for Azure service principal."
+  description = "The client secret for Azure authentication."
   type        = string
+  sensitive   = true  # Mark as sensitive to avoid displaying in logs
 }
 
 variable "tenant_id" {
-  description = "The tenant ID for Azure."
+  description = "The tenant ID for the Azure account."
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group."
+  description = "The name of the Azure resource group."
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region to deploy resources."
+  description = "The Azure location for resources."
   type        = string
 }
 
 variable "vm_name" {
   description = "The name of the virtual machine."
   type        = string
+}
+
+variable "vm_username" {
+  description = "The admin username for the virtual machine."
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the VM administrator account."
+  type        = string
+  sensitive   = true  # Mark as sensitive to avoid displaying in logs
+}
+
+variable "os_type" {
+  description = "The operating system type (linux or windows)."
+  type        = string
+  default     = "linux"  # Default can be set to either 'linux' or 'windows'
+}
+
+variable "image_source" {
+  description = "The source of the VM image (public or private)."
+  type        = string
+  default     = "public"
+}
+
+variable "private_image_id" {
+  description = "The ID of the private image to use if the image source is private."
+  type        = string
+  default     = ""  # Leave empty if not using a private image
 }
 
 variable "virtual_network" {
@@ -46,54 +76,42 @@ variable "subnet" {
 variable "vm_size" {
   description = "The size of the virtual machine."
   type        = string
+  default     = "Standard_DS1_v2"  # Example VM size
 }
 
-variable "vm_username" {
-  description = "The admin username for the VM."
-  type        = string
-}
-
-variable "password" {
-  description = "The admin password for the VM."
+variable "domain_name_label" {
+  description = "The domain name label for the public IP."
   type        = string
 }
 
 variable "type_of_storage" {
-  description = "The type of storage account for the OS disk."
+  description = "The type of storage for the OS disk (Standard_LRS, Premium_LRS, etc.)."
   type        = string
-}
-
-variable "image_source" {
-  description = "The source of the image (public/private)."
-  type        = string
-}
-
-variable "private_image_id" {
-  description = "The ID of the private image to use."
-  type        = string
+  default     = "Standard_LRS"  # Default storage type
 }
 
 variable "publisher" {
-  description = "Publisher of the public image."
+  description = "The publisher of the public image."
   type        = string
 }
 
 variable "offer" {
-  description = "Offer of the public image."
+  description = "The offer of the public image."
   type        = string
 }
 
 variable "sku" {
-  description = "SKU of the public image."
+  description = "The SKU of the public image."
   type        = string
 }
 
 variable "os_version" {
-  description = "OS version of the public image."
+  description = "The version of the public image."
   type        = string
 }
 
 variable "disk_size" {
-  description = "The size of the disk in GB."
+  description = "The size of the data disk in GB."
   type        = number
+  default     = 128  # Default disk size
 }

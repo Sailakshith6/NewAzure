3390dd9a9102020a4fd756bebdd390ebb1ecc15b
Sailakshith6 <117815467+Sailakshith6@users.noreply.github.com>
2024-11-05 22:18:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,11 +2,11 @@ output "os_disk_name" {
   value = var.os_type == "linux" ? azurerm_linux_virtual_machine.linux_example[0].os_disk[0].name : azurerm_windows_virtual_machine.windows_example[0].os_disk[0].name
 }
 
-output "vm_name" {
+output "virtual_machine_name" {
   value = var.os_type == "linux" ? azurerm_linux_virtual_machine.linux_example[0].name : azurerm_windows_virtual_machine.windows_example[0].name
 }
 
-output "public_ip_addresses" {
+output "public_ip_address" {
   value = azurerm_public_ip.example.ip_address  # Returns a list of all IP addresses
 }
 
@@ -18,7 +18,7 @@ output "private_ip_address" {
   value = azurerm_network_interface.example.private_ip_address
 }
 
-output "primary_dns_names" {
+output "primary_dns_name" {
   value = azurerm_public_ip.example.fqdn  # Returns a list of all FQDNs
 }
 

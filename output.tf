1b552a6a84b8712fb04f561bb2bfe1bcdeba4103
Sailakshith6 <117815467+Sailakshith6@users.noreply.github.com>
2024-11-05 16:54:24 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,9 @@
 output "os_disk_name" {
-  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].os_disk[0].name :
-          var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].os_disk[0].name :
-          var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].os_disk[0].name :
-          azurerm_windows_virtual_machine.windows_example_private[0].os_disk[0].name
+  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].os_disk[0].name : var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].os_disk[0].name : var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].os_disk[0].name : azurerm_windows_virtual_machine.windows_example_private[0].os_disk[0].name
 }
 
 output "virtual_machine_name" {
-  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].name :
-          var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].name :
-          var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].name :
-          azurerm_windows_virtual_machine.windows_example_private[0].name
+  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].name : var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].name : var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].name : azurerm_windows_virtual_machine.windows_example_private[0].name
 }
 
 output "public_ip_address" {
@@ -29,17 +23,11 @@ output "primary_dns_name" {
 }
 
 output "virtual_machine_id" {
-  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].id :
-          var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].id :
-          var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].id :
-          azurerm_windows_virtual_machine.windows_example_private[0].id
+  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].id : var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].id : var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].id : azurerm_windows_virtual_machine.windows_example_private[0].id
 }
 
 output "cloud_instance_id" {
-  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].id :
-          var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].id :
-          var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].id :
-          azurerm_windows_virtual_machine.windows_example_private[0].id
+  value = var.os_type == "linux" && var.image_source == "public" ? azurerm_linux_virtual_machine.linux_example_public[0].id : var.os_type == "linux" && var.image_source == "private" ? azurerm_linux_virtual_machine.linux_example_private[0].id : var.os_type == "windows" && var.image_source == "public" ? azurerm_windows_virtual_machine.windows_example_public[0].id : azurerm_windows_virtual_machine.windows_example_private[0].id
 }
 
 output "data_disk_name" {
